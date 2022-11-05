a92fb2e6c115c54271f7bf8d30830b9c52992f18
Bence Magyar <bence.magyar.robotics@gmail.com>
2022-11-05 08:44:07 +0000

    Rebase from 'upstream'

--- src/pid_ros.cpp
+++ src/pid_ros.cpp
@@ -1,38 +1,38 @@
-/*********************************************************************
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2020, Open Source Robotics Foundation, Inc.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of the Open Source Robotics Foundation nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *********************************************************************/
+// Copyright (c) 2020, Open Source Robotics Foundation, Inc.
+// All rights reserved.
+//
+// Software License Agreement (BSD License 2.0)
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//  * Neither the name of the Willow Garage nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 #include <algorithm>
 #include <cmath>
+#include <limits>
 #include <memory>
 #include <string>
 #include <utility>
@@ -45,15 +45,33 @@ namespace control_toolbox
 
 void PidROS::initialize(std::string topic_prefix)
 {
-  if (topic_prefix.back() != '.' && !topic_prefix.empty()) {
-    topic_prefix_ = topic_prefix + ".";
-  } else {
-    topic_prefix_ = topic_prefix;
+  param_prefix_ = topic_prefix;
+  // If it starts with a "~", remove it
+  if (param_prefix_.compare(0, 1, "~") == 0) {
+    param_prefix_.erase(0, 1);
+  }
+  // If it starts with a "/" or a "~/", remove those as well
+  if (param_prefix_.compare(0, 1, "/") == 0) {
+    param_prefix_.erase(0, 1);
+  }
+  // Replace namespacing separator from "/" to "." in parameters
+  std::replace(param_prefix_.begin(), param_prefix_.end(), '/', '.');
+  // Add a trailing "."
+  if (!param_prefix_.empty() && param_prefix_.back() != '.') {
+    param_prefix_.append(".");
+  }
+
+  topic_prefix_ = topic_prefix;
+  // Replace parameter separator from "." to "/" in topics
+  std::replace(topic_prefix_.begin(), topic_prefix_.end(), '.', '/');
+  // Add a trailing "/"
+  if (!topic_prefix_.empty() && topic_prefix_.back() != '/') {
+    topic_prefix_.append("/");
   }
 
   state_pub_ = rclcpp::create_publisher<control_msgs::msg::PidState>(
     topics_interface_,
-    topic_prefix + "/pid_state",
+    topic_prefix_ + "pid_state",
     rclcpp::SensorDataQoS());
   rt_state_pub_.reset(
     new realtime_tools::RealtimePublisher<control_msgs::msg::PidState>(state_pub_));
@@ -62,6 +80,7 @@ void PidROS::initialize(std::string topic_prefix)
 bool
 PidROS::getBooleanParam(const std::string & param_name, bool & value)
 {
+  declareParam(param_name, rclcpp::ParameterValue(value));
   rclcpp::Parameter param;
   if (node_params_->has_parameter(param_name)) {
     node_params_->get_parameter(param_name, param);
@@ -82,6 +101,7 @@ PidROS::getBooleanParam(const std::string & param_name, bool & value)
 bool
 PidROS::getDoubleParam(const std::string & param_name, double & value)
 {
+  declareParam(param_name, rclcpp::ParameterValue(value));
   rclcpp::Parameter param;
   if (node_params_->has_parameter(param_name)) {
     node_params_->get_parameter(param_name, param);
@@ -92,13 +112,13 @@ PidROS::getDoubleParam(const std::string & param_name, double & value)
       return false;
     }
     value = param.as_double();
-    RCLCPP_INFO_STREAM(
+    RCLCPP_DEBUG_STREAM(
       node_logging_->get_logger(),
       "parameter '" << param_name << "' in node '" << node_base_->get_name() <<
         "' value is " << value << std::endl);
     return true;
   } else {
-    RCLCPP_INFO_STREAM(
+    RCLCPP_ERROR_STREAM(
       node_logging_->get_logger(),
       "parameter '" << param_name << "' in node '" << node_base_->get_name() <<
         "' does not exists" << std::endl);
@@ -110,15 +130,16 @@ bool
 PidROS::initPid()
 {
   double p, i, d, i_min, i_max;
+  p = i = d = i_min = i_max = std::numeric_limits<double>::quiet_NaN();
   bool antiwindup = false;
   bool all_params_available = true;
-  all_params_available &= getDoubleParam(topic_prefix_ + "p", p);
-  all_params_available &= getDoubleParam(topic_prefix_ + "i", i);
-  all_params_available &= getDoubleParam(topic_prefix_ + "d", d);
-  all_params_available &= getDoubleParam(topic_prefix_ + "i_clamp_max", i_max);
-  all_params_available &= getDoubleParam(topic_prefix_ + "i_clamp_min", i_min);
+  all_params_available &= getDoubleParam(param_prefix_ + "p", p);
+  all_params_available &= getDoubleParam(param_prefix_ + "i", i);
+  all_params_available &= getDoubleParam(param_prefix_ + "d", d);
+  all_params_available &= getDoubleParam(param_prefix_ + "i_clamp_max", i_max);
+  all_params_available &= getDoubleParam(param_prefix_ + "i_clamp_min", i_min);
 
-  getBooleanParam(topic_prefix_ + "antiwindup", antiwindup);
+  getBooleanParam(param_prefix_ + "antiwindup", antiwindup);
 
   if (all_params_available) {
     setParameterEventCallback();
@@ -142,12 +163,12 @@ PidROS::initPid(double p, double i, double d, double i_max, double i_min, bool a
 {
   pid_.initPid(p, i, d, i_max, i_min, antiwindup);
 
-  declareParam("p", rclcpp::ParameterValue(p));
-  declareParam("i", rclcpp::ParameterValue(i));
-  declareParam("d", rclcpp::ParameterValue(d));
-  declareParam("i_clamp_max", rclcpp::ParameterValue(i_max));
-  declareParam("i_clamp_min", rclcpp::ParameterValue(i_min));
-  declareParam("antiwindup", rclcpp::ParameterValue(antiwindup));
+  declareParam(param_prefix_ + "p", rclcpp::ParameterValue(p));
+  declareParam(param_prefix_ + "i", rclcpp::ParameterValue(i));
+  declareParam(param_prefix_ + "d", rclcpp::ParameterValue(d));
+  declareParam(param_prefix_ + "i_clamp_max", rclcpp::ParameterValue(i_max));
+  declareParam(param_prefix_ + "i_clamp_min", rclcpp::ParameterValue(i_min));
+  declareParam(param_prefix_ + "antiwindup", rclcpp::ParameterValue(antiwindup));
 
   setParameterEventCallback();
 }
@@ -174,6 +195,14 @@ PidROS::computeCommand(double error, rclcpp::Duration dt)
   return cmd_;
 }
 
+double PidROS::computeCommand(double error, double error_dot, rclcpp::Duration dt)
+{
+  double cmd_ = pid_.computeCommand(error, error_dot, dt.nanoseconds());
+  publishPIDState(cmd_, error, dt);
+
+  return cmd_;
+}
+
 Pid::Gains
 PidROS::getGains()
 {
@@ -185,12 +214,12 @@ PidROS::setGains(double p, double i, double d, double i_max, double i_min, bool 
 {
   node_params_->set_parameters(
     {
-      rclcpp::Parameter(topic_prefix_ + "p", p),
-      rclcpp::Parameter(topic_prefix_ + "i", i),
-      rclcpp::Parameter(topic_prefix_ + "d", d),
-      rclcpp::Parameter(topic_prefix_ + "i_clamp_max", i_max),
-      rclcpp::Parameter(topic_prefix_ + "i_clamp_min", i_min),
-      rclcpp::Parameter(topic_prefix_ + "antiwindup", antiwindup)
+      rclcpp::Parameter(param_prefix_ + "p", p),
+      rclcpp::Parameter(param_prefix_ + "i", i),
+      rclcpp::Parameter(param_prefix_ + "d", d),
+      rclcpp::Parameter(param_prefix_ + "i_clamp_max", i_max),
+      rclcpp::Parameter(param_prefix_ + "i_clamp_min", i_min),
+      rclcpp::Parameter(param_prefix_ + "antiwindup", antiwindup)
     }
   );
 
@@ -287,25 +316,29 @@ PidROS::setParameterEventCallback()
 
       /// @note don't use getGains, it's rt
       Pid::Gains gains = pid_.getGains();
+      bool changed = false;
 
       for (auto & parameter : parameters) {
         const std::string param_name = parameter.get_name();
         try {
-          if (param_name == "p") {
+          if (param_name == param_prefix_ + "p") {
             gains.p_gain_ = parameter.get_value<double>();
-          } else if (param_name == "i") {
+            changed = true;
+          } else if (param_name == param_prefix_ + "i") {
             gains.i_gain_ = parameter.get_value<double>();
-          } else if (param_name == "d") {
+            changed = true;
+          } else if (param_name == param_prefix_ + "d") {
             gains.d_gain_ = parameter.get_value<double>();
-          } else if (param_name == "i_clamp_max") {
+            changed = true;
+          } else if (param_name == param_prefix_ + "i_clamp_max") {
             gains.i_max_ = parameter.get_value<double>();
-          } else if (param_name == "i_clamp_min") {
+            changed = true;
+          } else if (param_name == param_prefix_ + "i_clamp_min") {
             gains.i_min_ = parameter.get_value<double>();
-          } else if (param_name == "antiwindup") {
+            changed = true;
+          } else if (param_name == param_prefix_ + "antiwindup") {
             gains.antiwindup_ = parameter.get_value<bool>();
-          } else {
-            result.successful = false;
-            result.reason = "Invalid parameter";
+            changed = true;
           }
         } catch (const rclcpp::exceptions::InvalidParameterTypeException & e) {
           RCLCPP_INFO_STREAM(
@@ -313,14 +346,15 @@ PidROS::setParameterEventCallback()
         }
       }
 
-      if (result.successful) {
+      if (changed) {
         /// @note don't call setGains() from inside a callback
         pid_.setGains(gains);
       }
 
       return result;
     };
-
+  /// @note this gets called whenever a parameter changes.
+  /// Any parameter under that node. Not just PidROS.
   parameter_callback_ =
     node_params_->add_on_set_parameters_callback(
     on_parameter_event_callback);
